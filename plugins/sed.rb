f7f9fdd70d44dbdc026c0e95edbad077a7bc373a
Willem Mulder <willemmaster@hotmail.com>
2015-11-17 19:54:32 +0100

    Fix escaping of separators, improve debug logging

--- plugins/sed.rb
+++ plugins/sed.rb
@@ -1,6 +1,6 @@
 require "hashie"
 
-sed_regex = /^s(?<sep>[^[:alnum:]\(\[<[:space:]])(?<regex>.*?)(?<!(?:[^\\]\\)*)\k<sep>(?<replace>.*?)(?<!(?:[^\\]\\)*)\k<sep>(?<flags>.*)/
+sed_regex = /^s(?<sep>[^[:alnum:](\[<[:space:]])(?<regex>(?:\\.|[^\\])*?)\k<sep>(?<replace>(?:\\.|[^\\])*?)\k<sep>(?<flags>[a-z]*)$/
 
 line_hist = Hash.new {|hash, key| hash[key] = Array.new}
 
@@ -8,21 +8,22 @@ always do |bot, msg|
   $log.debug('sed') { "Such message: #{msg.text}\n" }
   broken = false
   if m = sed_regex.match(msg.text)
-    $log.debug('sed') { "They don't think it match, but it do\n" }
+    $log.debug('sed') { "They don't think it match, but it do: #{m.inspect}" }
     global = m[:flags].include? 'g'
     flags  = Regexp::IGNORECASE if m[:flags].include? 'i'
     flags |= Regexp::EXTENDED   if m[:flags].include? 'x'
     flags |= Regexp::MULTILINE  if m[:flags].include? 'm'
 
     begin
-      regex = Regexp.new m[:regex], flags
+      r = m[:regex].gsub /\\#{m[:sep]}/, m[:sep]
+      regex = Regexp.new r, flags
     rescue RegexpError => e
       bot.api.send_message chat_id:             msg.chat.id,
                            text:                "What's this I don't even (#{e})",
                            reply_to_message_id: msg.message_id
     end
 
-    replace = m[:replace].gsub /(?<!(?:[^\\]\\)*)\\#{m[:sep]}/, m[:sep]
+    replace = m[:replace].gsub /\\#{m[:sep]}/, m[:sep]
 
     line_hist[msg.chat.id].each do |entry|
       if regex.match entry.msg
