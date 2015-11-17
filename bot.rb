6e10e97e99bc9e6a9d304264d31183a5a00b0b80
Willem Mulder <14mRh4X0r@gmail.com>
2015-11-17 16:28:10 +0100

    Add more ways to invoke commands

--- bot.rb
+++ bot.rb
@@ -39,34 +39,36 @@ end
 
 require_relative 'plugins'
 
+def process bot, msg
+  $log.debug "Got a msg: #{msg.inspect}"
+  if msg.text and msg.text[0] == '/'
+    cmd = msg.text.split[0][1..-1]
+    cmd, who = cmd.split '@' if cmd.include? '@'
+    $log.debug "Parsing command: #{msg.text.inspect}, #{cmd.inspect}, #{who.inspect}"
+    $commands[cmd].call(bot, msg) if $commands.has_key? cmd and (who.nil? or who.eql? me.username)
+  end
+
+  $matches.each do |regex, block|
+    regex.match(msg.text) do |match|
+      block.call(bot, msg, match)
+    end if msg.text
+  end
+
+  $replies.each do |id, block|
+    if id == msg.reply_to_message.message_id
+      block.call(bot, msg)
+      $replies.delete id
+    end
+  end if msg.reply_to_message
+
+  $always.each {|block| block.call(bot,msg)}
+end
+
 Telegram::Bot::Client.run(TOKEN) do |bot|
   begin
-    me = Hashie::Mash.new(bot.api.get_me).result
+    $me = Hashie::Mash.new(bot.api.get_me).result
     $log.debug "I am #{me.username}"
-    bot.listen do |msg|
-      $log.debug "Got a msg: #{msg.inspect}"
-      if msg.text and msg.text[0] == '/'
-        cmd = msg.text.split[0][1..-1]
-        cmd, who = cmd.split '@' if cmd.include? '@'
-        $log.debug "Parsing command: #{msg.text.inspect}, #{cmd.inspect}, #{who.inspect}"
-        $commands[cmd].call(bot, msg) if $commands.has_key? cmd and (who.nil? or who.eql? me.username)
-      end
-
-      $matches.each do |regex, block|
-        regex.match(msg.text) do |match|
-          block.call(bot, msg, match)
-        end if msg.text
-      end
-
-      $replies.each do |id, block|
-        if id == msg.reply_to_message.message_id
-          block.call(bot, msg)
-          $replies.delete id
-        end
-      end if msg.reply_to_message
-
-      $always.each {|block| block.call(bot,msg)}
-    end
+    bot.listen {|msg| process bot, msg}
   rescue Interrupt
     $log.warn "Caught interrupt -- quitting"
   end
---
+++ plugins/addressing.rb
@@ -0,0 +1,14 @@
+require 'hashie'
+require 'telegram/bot/types/message'
+
+def subst_forward re
+  match re do |bot, msg, match|
+    new_msg = Telegram::Bot::Types::Message.new msg.attributes
+    new_msg.text = msg.text.sub re, '/'
+
+    process bot, new_msg
+  end
+end
+
+subst_forward /^!/
+subst_forward /^@#{$me.username}[:,]?\s+/
