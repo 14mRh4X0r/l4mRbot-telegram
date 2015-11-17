d4b2da0a1e9ecc89ec9e201f35b354193d481be2
Willem Mulder <willemmaster@hotmail.com>
2015-11-17 17:06:34 +0100

    Fix addressing's message cloning, adjust require

--- plugins/addressing.rb
+++ plugins/addressing.rb
@@ -4,6 +4,7 @@ require 'telegram/bot/types/message'
 def subst_forward re
   match re do |bot, msg, match|
     new_msg = Telegram::Bot::Types::Message.new msg.attributes
+    new_msg.instance_variable_set :@chat, msg.chat
     new_msg.text = msg.text.sub re, '/'
 
     process bot, new_msg
