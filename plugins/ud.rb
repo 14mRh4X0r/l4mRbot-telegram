b88773daba048c4f6f92c359e4a00455ecd2aee0
Willem Mulder <14mRh4X0r@gmail.com>
2016-06-20 11:17:53 +0200

    Rescue from errors due to e.g. timeouts

--- plugins/ud.rb
+++ plugins/ud.rb
@@ -1,15 +1,20 @@
 require 'ud'
 
 command 'ud' do |bot, msg|
-  if msg.text.split.length < 2
-    ret = Hashie::Mash.new bot.api.send_message(chat_id: msg.chat.id,
-                                                text: "What word would you like me to look up?",
-                                                reply_to_message_id: msg.message_id,
-                                                reply_markup: Telegram::Bot::Types::ForceReply.new(force_reply: true,
-                                                                                                   selective: true))
-    reply(ret.result.message_id)  {|bot, msg| do_query bot, msg}
-  else
-    do_query bot, msg
+  begin
+    if msg.text.split.length < 2
+      ret = Hashie::Mash.new bot.api.send_message(chat_id: msg.chat.id,
+                                                  text: "What word would you like me to look up?",
+                                                  reply_to_message_id: msg.message_id,
+                                                  reply_markup: Telegram::Bot::Types::ForceReply.new(force_reply: true,
+                                                                                                     selective: true))
+      reply(ret.result.message_id)  {|bot, msg| do_query bot, msg}
+    else
+      do_query bot, msg
+    end
+  rescue Telegram::Bot::Exceptions::ResponseError => e
+    $log.error('ud') { "Failed to ask the word to look up: #{e}. Backtrace:" }
+    $log.error('ud') { e.backtrace.join "\n" }
   end
 end
 
@@ -29,10 +34,14 @@ def do_query bot, msg
     end.join
   rescue
     "Something went horribly wrong."
-  end
+  end.force_encoding "utf-8"
   text = "_No results._" if text.nil? || text.empty?
+
   bot.api.send_message chat_id: msg.chat.id,
                        text: text,
                        parse_mode: "Markdown",
                        reply_to_message_id: msg.message_id
+rescue Telegram::Bot::Exceptions::ResponseError => e
+    $log.error('ud') { "Failed to execute query: #{e}. Backtrace:" }
+    $log.error('ud') { e.backtrace.join "\n" }
 end
